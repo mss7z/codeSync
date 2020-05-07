250b2f00bf3ff27c0ae29a6f35de4668163136f0
mss7z <49343918+mss7z@users.noreply.github.com>
2020-05-08 03:18:57 +0900

    fix run.bat test folder

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ echo starting compile ...
 make debug -j2
 if %errorlevel%==0 (
 	echo finished !
-	codeSync.exe D:\00_C_Cpp_Program\codeShare\tests
+	codeSync.exe tests
 ) else (
 	echo failed to compile
 )
